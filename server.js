35aaeeb959cfd8c2f2ee0d3fe950d6330c7ad150
gloryglara-dotcom <gloryglara@gmail.com>
2025-12-17 11:45:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const app = express();
 app.use(express.json());
 
 let projects = [];
+let tasks = [];
 
-app.post("/project", (req, res) => {
-  projects.push(req.body);
-  res.json({ message: "Project created" });
+app.post("/project",(req,res)=>{
+ projects.push(req.body);
+ res.json({message:"Project created"});
 });
 
-app.get("/projects", (req, res) => {
-  res.json(projects);
+app.post("/task",(req,res)=>{
+ tasks.push(req.body);
+ res.json({message:"Task assigned"});
 });
 
-app.listen(3002, () => {
-  console.log("Project tool running on port 3002");
+app.get("/projects",(req,res)=>{
+ res.json(projects);
 });
+
+app.listen(3002,()=>console.log("Project tool running"));
